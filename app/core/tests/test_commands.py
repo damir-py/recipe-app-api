d9f4848fa78cf43219b02c8fc71543a742c56c7f
Damir <nimadjonov.2404@gmail.com>
2024-12-05 19:04:01 +0500

    Changed handle function to waiting the database

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -31,4 +31,4 @@ class CommandTEsts(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patch_ckeck.assert_called_with(databases=['default'])
+        patched_check.assert_called_with(databases=['default'])
