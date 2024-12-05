1c00698f470f381def7c4f862fc0591ef5365dbf
Damir <nimadjonov.2404@gmail.com>
2024-12-05 18:00:12 +0500

    added testing coded

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,6 +4,8 @@ Test custom Django management commands.
 
 from unittest.mock import patch
 
+
+from django.core.management import call_command
 from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
