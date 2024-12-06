ecb56f0ee73c8244fd2a790e976ea7cdbd1b0142
Damir <nimadjonov.2404@gmail.com>
2024-12-06 17:32:36 +0500

    removed unnessesry sapes in the code

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@
 calculator functions
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
     return x + y
@@ -9,5 +10,5 @@ def add(x, y):
 
 def subtract(x, y):
     """Subtract x from y and return result."""
-    
+
     return y - x
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,15 +1,16 @@
 """
 Django command to wait for the database to be available.
 """
-
 import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
+    """Django command for waiting database."""
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database....')
         db_up = False
@@ -21,4 +22,3 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-        
