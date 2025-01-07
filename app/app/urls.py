f887d65231fe404581057dd6c226a6a972c51801
Damir <nimadjonov.2404@gmail.com>
2025-01-07 15:37:11 +0500

    added a new app to project

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,6 +22,7 @@ from django.contrib import admin
 from django.urls import path
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
