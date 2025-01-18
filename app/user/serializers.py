8553bd2e16c837e636390d3688ba82b5823bac86
Damir <nimadjonov.2404@gmail.com>
2025-01-18 17:54:40 +0500

    adde new lise in files

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,7 +3,7 @@ Serializers for the user API View.
 """
 from django.contrib.auth import (
     get_user_model,
-    authenticate
+    authenticate,
 )
 from django.utils.translation import gettext as _
 
@@ -22,6 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
     email = serializers.EmailField()
@@ -35,9 +36,9 @@ class AuthTokenSerializer(serializers.Serializer):
         email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
-            request = self.context.get('request'),
-            username = email,
-            password = password,
+            request=self.context.get('request'),
+            username=email,
+            password=password,
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials.')
